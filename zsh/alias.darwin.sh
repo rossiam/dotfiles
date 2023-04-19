7c263292c90504ad1f45a2b7f4a0be6b7a442727
Ross Stenersen <rossiam@gmail.com>
2023-04-19 10:05:40 -0500

    add linux aliases

--- zsh/alias.darwin.sh
+++ zsh/alias.darwin.sh
@@ -3,4 +3,5 @@ alias prusa='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/Mac
 #alias green='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Dropbox/Documents/hacking/green_lantern/PrusaSlicer'
 alias green='/Applications/SuperSlicer.app/Contents/MacOS/SuperSlicer -datadir /Users/rossiam/Dropbox/Documents/hacking/green_lantern/rusaSlicer'
 
+alias vcc="vi ${HOME}/Library/Caches/@smartthings/cli/config.yaml"
 alias vcmc="vi ${HOME}/Library/Caches/@smartthings/cli/config-managed.yaml"
---
+++ zsh/alias.linux.sh
@@ -0,0 +1,2 @@
+alias vcc="vi ${HOME}/.config/@smartthings/cli/config.yaml"
+alias vcmc="vi ${HOME}/.config/@smartthings/cli/config-managed.yaml"
