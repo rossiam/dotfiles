0961767b62a40d0c7f9042909370320325476b24
Ross Stenersen <rossiam@gmail.com>
2025-12-04 17:51:52 -0600

    minor updates

--- zsh/alias.darwin.sh
+++ zsh/alias.darwin.sh
@@ -1,13 +1,17 @@
 alias railcore='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/railcore/PrusaSlicer'
 #alias prusa='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer'
 alias mk3='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_MK3'
-alias mk4='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_MK4'
+# alias mk4='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_MK4'
+alias mk4s='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_MK4S'
+alias core1='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_CoreOne'
 alias xl='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_XL'
 alias xl-old='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_XL-old'
 alias cocoa='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/CocoaPress'
 #alias green='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/green_lantern/PrusaSlicer'
-alias green='/Applications/SuperSlicer.app/Contents/MacOS/SuperSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/green_lantern/rusaSlicer'
+alias green='/Applications/SuperSlicer.app/Contents/MacOS/SuperSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/green_lantern/prusaSlicer'
 alias blue='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/PrusaSlicer_Voron0_Blue'
+alias miles-voron='/Applications/Original\ Prusa\ Drivers/PrusaSlicer.app/Contents/MacOS/PrusaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/prusa_slicer_profiles/miles-voron'
+alias orca-miles='/Applications/OrcaSlicer.app/Contents/MacOS/OrcaSlicer -datadir /Users/rossiam/Tresorit/Documents/hacking/orca_slicer_profiles/miles-voron'
 
 alias vcc="vi ${HOME}/.config/@smartthings/cli/config.yaml"
 alias vcmc="vi ${HOME}/Library/Caches/@smartthings/cli/config-managed.yaml"
