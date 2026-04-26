f1050af26a21cec5529983b431be09daee522585
Ross Stenersen <rossiam@gmail.com>
2026-04-26 11:39:59 -0500

    add slicer aliases for linux

--- zsh/alias.linux.sh
+++ zsh/alias.linux.sh
@@ -1,2 +1,17 @@
 alias vcc="vi ${HOME}/.config/@smartthings/cli/config.yaml"
 alias vcmc="vi ${HOME}/.config/@smartthings/cli/config-managed.yaml"
+
+prusa_slicer=prusa-slicer
+slicer_profile_dir="${HOME}/Tresorit/Documents/hacking"
+
+alias railcore="prusa-slicer -datadir ${slicer_profile_dir}/railcore/PrusaSlicer"
+alias mk3="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_MK3"
+alias mk4s="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_MK4S"
+alias core1="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_CoreOne"
+alias xl="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_XL"
+alias xl-old="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_XL-old"
+alias cocoa="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/CocoaPress"
+# alias green="/Applications/SuperSlicer.app/Contents/MacOS/SuperSlicer -datadir ${slicer_profile_dir}/green_lantern/prusaSlicer"
+alias blue="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/PrusaSlicer_Voron0_Blue"
+alias miles-voron="prusa-slicer -datadir ${slicer_profile_dir}/prusa_slicer_profiles/miles-voron"
+# alias orca-miles="/Applications/OrcaSlicer.app/Contents/MacOS/OrcaSlicer -datadir ${slicer_profile_dir}/orca_slicer_profiles/miles-voron"
