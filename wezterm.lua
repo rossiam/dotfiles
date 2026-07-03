78f972f65696a67b8e144c5eaac013be3838ef51
Ross Stenersen <rossiam@gmail.com>
2026-07-03 13:44:04 -0500

    wezterm updates

--- wezterm.lua
+++ wezterm.lua
@@ -2,21 +2,20 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-
+config.enable_scroll_bar = true
 config.initial_cols = 120
 config.initial_rows = 30
-
 config.font_size = 14
 
 config.color_scheme = 'AdventureTime'
 
 config.keys = {
-		{
-			key = 'k',
-			mods = 'CTRL|SHIFT',
-			action = wezterm.action.Multiple {
-				wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-				wezterm.action.SendKey { key = 'l', mods = 'CTRL' },
+	{
+		key = 'k',
+		mods = 'CTRL|SHIFT',
+		action = wezterm.action.Multiple {
+			wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+			wezterm.action.SendKey { key = 'l', mods = 'CTRL' },
 		},
 	},
 }
